0ddb3cfee53a812573091c001be98e59d62f22a9
Abhinav Chourasia <abhinavchourasia@qre0ct.local>
2020-03-26 13:48:39 +0530

    first commit for AppAwesome-backend-CSP-inlineJS

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from flask import request, jsonify, redirect
 from flask import make_response
 import os
 
-_name_ = "AppAwesome"
+_name_ = "AppAwesomeCSP"
 template_dir = os.path.abspath('static/template')
 app     = Flask(_name_, template_folder=template_dir)
+
+
+@app.after_request
+def apply_csp(response):
+    response.headers.set('Content-Security-Policy', "script-src 'self' http://192.168.1.35:31337; frame-ancestors 'self' http://192.168.1.35:31337")
+    return response
+
+@app.after_request
+def apply_otherHeaders(response):
+    response.headers.set('Cache-Control', "no-cache, max-age=0, must-revalidate, no-store")        
+    return response    
 
 @app.route("/home")
 def home():      
