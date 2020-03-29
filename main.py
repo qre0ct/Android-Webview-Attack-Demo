1ceb7950ed9da3146c31977302e596c64ab9e8e2
Abhinav Chourasia <abhinavchourasia@qre0ct.local>
2020-03-29 12:52:12 +0530

    made it similar to the older examples for consistency

--- main.py
+++ main.py
@@ -8,21 +8,26 @@ _name_ = "AppAwesomeBackend FullBlown"
 template_dir = os.path.abspath('static/template')
 app     = Flask(_name_, template_folder=template_dir)
 
-@app.route("/home")
-def index():      
-   return app.send_static_file('parent.html')
-
 @app.after_request
 def apply_otherHeaders(response):
-    response.headers.set('Cache-Control', "no-cache, max-age=0, must-revalidate, no-store")        
-    response.headers.set('X-XSS-Protection', "0")
-    return response
+  response.headers.set('Cache-Control', "no-cache, max-age=0, must-revalidate, no-store")        
+  response.headers.set('X-XSS-Protection', "0")
+  return response
 
-@app.route("/child", methods=['GET'])
-def child():      
-   userInput = request.args.get('secret')
-   r = make_response(render_template('child.html', ui=userInput))
-   return r   
+@app.route("/home")
+def index():      
+  return app.send_static_file('parent.html')
+
+@app.route("/child")
+def child():         
+  return app.send_static_file('child.html')
+
+
+@app.route("/checkout", methods=['GET'])
+def checkout():
+  userInput = request.args.get('password')
+  r = make_response(render_template('checkout.html', ui=userInput))
+  return r 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="192.168.1.34", port=31337)
+  app.run(debug=True, host="192.168.1.34", port=31337)
