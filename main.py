4c50f7e28eb54d8a64a898d87e7c98fab075c293
Abhinav Chourasia <abhinavchourasia@qre0ct.local>
2020-03-29 11:58:55 +0530

    First commit for AppAwesome-backend-FullBlown-noCSP-iframeSandbox

--- main.py
+++ main.py
@@ -3,30 +3,26 @@ from flask import request, jsonify, redirect
 from flask import make_response
 import os
 
-_name_ = "AppAwesomeCSP"
+_name_ = "AppAwesomeBackend FullBlown"
+
 template_dir = os.path.abspath('static/template')
 app     = Flask(_name_, template_folder=template_dir)
 
-
-@app.after_request
-def apply_csp(response):
-    response.headers.set('Content-Security-Policy', "script-src 'self' http://192.168.1.35:31337; frame-ancestors 'self' http://192.168.1.35:31337")
-    return response
+@app.route("/home")
+def index():      
+   return app.send_static_file('parent.html')
 
 @app.after_request
 def apply_otherHeaders(response):
     response.headers.set('Cache-Control', "no-cache, max-age=0, must-revalidate, no-store")        
-    return response    
+    response.headers.set('X-XSS-Protection', "0")
+    return response
 
-@app.route("/home")
-def home():      
-  return app.send_static_file('appAwesomeCheckout.html')
-
-
-@app.route("/checkout")
-def checkout():      
-  return render_template('checkout.html')  
-   
+@app.route("/child", methods=['GET'])
+def child():      
+   userInput = request.args.get('secret')
+   r = make_response(render_template('child.html', ui=userInput))
+   return r   
 
 if __name__ == '__main__':
-  app.run(debug=True, host="192.168.1.35", port=31337)
+    app.run(debug=True, host="192.168.1.34", port=31337)
